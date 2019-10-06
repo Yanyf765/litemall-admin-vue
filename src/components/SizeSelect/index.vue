693a033110b96588c46878212d868924961741c1
yangzheng1 <1069958530@qq.com>
2019-10-06 22:15:54 +0800

    首页tagsview完成

--- src/components/SizeSelect/index.vue
+++ src/components/SizeSelect/index.vue
@@ -1,13 +1,50 @@
 <template>
-    <div>SizeSelect</div>
+   <el-dropdown trigger="click" @command="handleSetSize">
+    <div>
+      <svg-icon class-name="size-icon" icon-class="size"></svg-icon>
+    </div>
+    <el-dropdown-menu slot="dropdown">
+      <el-dropdown-item :disabled="size==='medium'" command="medium">Medium</el-dropdown-item>
+      <el-dropdown-item :disabled="size==='small'" command="small">Small</el-dropdown-item>
+      <el-dropdown-item :disabled="size==='mini'" command="mini">Mini</el-dropdown-item>
+    </el-dropdown-menu>
+   </el-dropdown>
 </template>
 
 <script>
 export default {
-  name: 'index'
+  computed: {
+    size () {
+      return this.$store.getters.size
+    }
+  },
+  methods: {
+    handleSetSize (size) {
+      this.$ELEMENT.size = size
+      this.$store.dispatch('setSize', size)
+      this.refreshView()
+      this.$message({
+        message: '布局尺寸切换成功',
+        type: 'success'
+      })
+    },
+    refreshView () {
+      this.$store.dispatch('delAllCachedViews', this.$route)
+      const {fullPath} = this.$route
+      this.$nextTick(() => {
+        this.$router.replace({
+          path: '/redirect' + fullPath
+        })
+      })
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.size-icon{
+  font-size: 20px;
+  cursor: pointer;
+  vertical-align: -4px!important;
+}
 </style>
