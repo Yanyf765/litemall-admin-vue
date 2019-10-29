4dad97d4e77c5abfa4b5c5999225a40257dc5958
Administrator <yangzheng@himobi.net>
2019-10-29 23:29:28 +0800

    商品评论

--- src/views/goods/comment.vue
+++ src/views/goods/comment.vue
@@ -29,32 +29,107 @@
       </template>
     </el-table-column>
   </el-table>
+
+  <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
+
+  <!--评论回复-->
+  <el-dialog :visible.sync="replyFormVisible" title="回复">
+    <el-form ref="replyForm" :model="replyForm" status-icon label-position="left" label-width="100px" style="width: 400px;">
+      <el-form-item label="回复内容" prop="content">
+        <el-input :autosize="{minRows: 4, maxRows: 8}" v-model="replyForm.content" type="textarea"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="replyFormVisible = false">取消</el-button>
+      <el-button type="primary" @click="reply">确定</el-button>
+    </div>
+  </el-dialog>
 </div>
 </template>
 
 <script>
+import {listComment, deleteComment} from '@/api/comment'
+import {replyComment} from '@/api/order'
+import Pagination from '@/components/Pagination'
 export default {
   name: 'comment',
+  components: {Pagination},
   data () {
     return {
       list: [],
+      total: 0,
       downloadLoading: false,
       listQuery: {
         userId: undefined,
         valueId: undefined
       },
-      listLoading: false
+      listLoading: false,
+      replyForm: {
+        commentId: 0,
+        content: ''
+      },
+      replyFormVisible: false
     }
   },
+  created () {
+    this.getList()
+  },
   methods: {
+    getList () {
+      this.listLoading = true
+      listComment(this.listQuery).then(response => {
+        this.list = response.data.data.list
+        this.total = response.data.data.total
+        this.listLoading = false
+      }).catch(() => {
+        this.list = []
+        this.total = 0
+        this.listLoading = false
+      })
+    },
     handleFilter () {
-
+      this.listQuery.page = 1
+      this.getList()
     },
     handleDownload () {
+      this.downloadLoading = true
+      import('@/vendor/Export2Excel').then(excel => {
+        const tHeader = ['评论ID', '用户ID', '商品ID', '评论', '评论图片列表', '评论时间']
+        const filterVal = ['id', 'userId', 'valueId', 'content', 'picUrls', 'addTime']
+        excel.export_json_to_excel2(tHeader, this.list, filterVal, '商品评论信息')
+        this.downloadLoading = false
+      })
     },
     handleReply (row) {
+      this.replyFormVisible = true
+      this.replyForm = {commentId: row.id, content: ''}
     },
     handleDelete (row) {
+      deleteComment(row).then(response => {
+        this.replyFormVisible = false
+        this.$notify({
+          title: '成功',
+          message: '删除成功',
+          type: 'success',
+          duration: 2000
+        })
+        const index = this.list.indexOf(row)
+        this.list.splice(index, 1)
+      })
+    },
+    reply () {
+      replyComment(this.replyForm).then(response => {
+        this.replyFormVisible = false
+        this.$notify.success({
+          title: '成功',
+          message: '回复成功'
+        })
+      }).catch(response => {
+        this.$notify.error({
+          title: '失败',
+          message: response.data.errmsg
+        })
+      })
     }
   }
 }
